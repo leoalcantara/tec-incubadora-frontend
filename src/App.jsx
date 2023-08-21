4543e0839270267b80979689387e8f5d2ff512e8
unknown <leonidio.ti@gmail.com>
2023-08-21 15:42:45 -0300

    Project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Header } from './components/Header'
+
+function App() {
+  
+  return (
+    <>
+    <Header/>
+      <h1>Hello World!!!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+
+
+export function Header(options) {
+    return (
+        <header>teste</header>
+    )
+}
