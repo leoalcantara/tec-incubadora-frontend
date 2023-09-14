1b4972223fcafe0ff3789b4052e3a0c230a5bf05
unknown <leonidio.ti@gmail.com>
2023-09-13 23:40:20 -0300

    feature: Insert icons (Certification, Companies, Mentoring and Jobs)

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,71 @@
-import { ErrorBox } from './components/ErrorBox';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
+import { ErrorBox } from "./components/ErrorBox"
+import { Footer } from "./components/Footer"
+import { Header } from "./components/Header"
 
-import styles from './App.module.css'
+import styles from "./App.module.css"
 
-import ProgramImage from './assets/programa-de-incubacao.png'
+import ProgramImage from "./assets/programa-de-incubacao.png"
+import CertificationIcon from "./assets/icons/certificado.svg"
+import JobsIcon from "./assets/icons/empregos.svg"
+import MentorIngIcon from "./assets/icons/mentoria.svg"
+import CompaniesIcon from "./assets/icons/empresas.svg"
 
-
-import './global.css';
+import "./global.css"
 
 function App() {
-  
   return (
     <div>
-      <Header/>  
+      <Header />
 
-      <div className={styles.aboutBox}>
-        <div className={styles.aboutContent}>
+      <div className={styles.aboutContainer}>
+        <div className={styles.aboutBox}>
           <div>
-
+            <div className={styles.circle}>
+              <img src={CertificationIcon} alt="" />
+            </div>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>        
-      </div> 
+          <div>
+            <div className={styles.circle}>
+              <img src={CompaniesIcon} alt="" />
+            </div>
+          </div>
+          <div>
+            <div className={styles.circle}>
+              <img src={MentorIngIcon} alt="" />
+            </div>
+          </div>
+          <div>
+            <div className={styles.circle}>
+              <img src={JobsIcon} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div className={styles.container}>
-          <div className={styles.box}>
-            <div>
-              <img src={ProgramImage} alt="" />
-            </div>
-            <div>
-              <h2>Programa de Incubação</h2>
-              <span>
-                  É através do programa que projetos e ideias <br/> da área de tecnologia são apresentados <br/> e se transformam em startups. 
-              </span>
-              <ul>
-                <li>Aprenda com empreendedores de sucesso</li>
-                <li>Mentorias exclusivas e personalizadas</li>
-                <li>Taxa de 80% de sobrevivência no mercado</li>
-                <li>Networking de alto nível</li>
-              </ul>
-
-              <button>Conheça mais</button>
-            </div>
-                
+        <div className={styles.box}>
+          <div>
+            <img src={ProgramImage} alt="" />
           </div>
+          <div>
+            <h2>Programa de Incubação</h2>
+            <span>
+              É através do programa que projetos e ideias <br /> da área de
+              tecnologia são apresentados <br /> e se transformam em startups.
+            </span>
+            <ul>
+              <li>Aprenda com empreendedores de sucesso</li>
+              <li>Mentorias exclusivas e personalizadas</li>
+              <li>Taxa de 80% de sobrevivência no mercado</li>
+              <li>Networking de alto nível</li>
+            </ul>
 
+            <button>Conheça mais</button>
+          </div>
+        </div>
       </div>
-     
-      <Footer/>
-       
+
+      <Footer />
     </div>
   )
 }
