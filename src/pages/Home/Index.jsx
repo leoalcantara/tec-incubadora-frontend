9c01a23ed48d8bf9dea3048cbd6f560c26c6ac9a
unknown <leonidio.ti@gmail.com>
2023-10-05 15:26:23 -0300

    contact session started

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,13 +1,15 @@
 import { About } from "../../components/About";
 import {Services} from "../../components/Services/Index";
 import {IncubationProgram} from "../../components/IncubationProgram";
+import { Contact } from "../../components/Contact";
 
 export function Home() {
   return (
     <>        
       <About/> 
       <IncubationProgram/>  
-      <Services/>    
+      <Services/> 
+      <Contact/>   
     </> 
    
   );
