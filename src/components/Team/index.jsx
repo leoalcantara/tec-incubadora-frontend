d1de5625ff8530b8dad14c80a440051f9f490001
unknown <leonidio.ti@gmail.com>
2023-11-21 08:05:16 -0300

    url avatar

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,11 +1,17 @@
 import { TeamCard } from "../TeamCard";
 
 
+import avatar1 from "../../assets/avatar-1.png";
+import avatar2 from "../../assets/avatar-2.png";
+import avatar3 from "../../assets/avatar-3.png";
+import avatar4 from "../../assets/avatar-4.png";
+import avatar5 from "../../assets/avatar-5.png";
+import avatar6 from "../../assets/avatar-6.png";
 
 
 import styles from './Team.module.css';
 
-
+console.log(avatar1);
 export function Team() {
     return (
         <>
@@ -17,22 +23,28 @@ export function Team() {
 
                     <div className={styles.content}>
                     <TeamCard 
-                        
+                        URLAvatar={avatar1}
+                        Name={'Henrique da Hora'}
                     />
                     <TeamCard 
-                        
+                         URLAvatar={avatar2}
+                         Name={'Edson Terra'}
                     />
                     <TeamCard 
-                        
+                        URLAvatar={avatar3}
+                        Name={'Luiz Gustavo'}
                     />
                     <TeamCard 
-                        
+                         URLAvatar={avatar4}
+                         Name={'Rodrigo Lira'}
                     />
                     <TeamCard 
-                        
+                         URLAvatar={avatar5}
+                         Name={'Adriana Crespo'}
                     />
                     <TeamCard 
-                            
+                         URLAvatar={avatar6}  
+                         Name={'Túlio Baita'}  
                     />
                 </div>                   
                 </div>
--- src/components/TeamCard/index.jsx
+++ src/components/TeamCard/index.jsx
@@ -1,21 +1,21 @@
 import styles from "./TeamCard.module.css";
 
-import avatar1 from "../../assets/avatar-1.png";
+
 
 import InstagramIcon from '../../assets/icons/icone-insta.svg';
 import LinkedinIcon from '../../assets/icons/icone-linkedin.svg';
 
 
-export function TeamCard(){
+export function TeamCard(props){
     return (
         <>
         <div className={styles.card}>
         <div className={styles.avatar}>
-                    <img src={avatar1} alt="" />
+                    <img src={props.URLAvatar} alt="" />
                 </div>
             <div className={styles.header}>            
                 
-                <h3>Henrique da Hora</h3>
+                <h3>{props.Name}</h3>
                 <p>Presidente</p>
                 
                 <div className={styles.socialIcons}>
