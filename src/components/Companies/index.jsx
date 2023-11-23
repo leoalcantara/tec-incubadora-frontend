0b500798ca2e8797bd052ec4db8007d2fe18375b
unknown <leonidio.ti@gmail.com>
2023-11-23 15:47:47 -0300

    ajustar css do rodapé

--- src/components/Companies/index.jsx
+++ src/components/Companies/index.jsx
@@ -4,11 +4,15 @@ import styles from './Companies.module.css';
 export function Companies() {
     return (
         <section className={styles.container}>
-            <div className={styles.box}>
-                <div className={styles.header}>                  
+            <div className={styles.header}>  
+                <div>
                     <h1 className={styles.title}>Empresas Graduadas</h1>
                     <p>Conheça as empresas que estão incubadas em nossa instituição</p>
-                </div> 
+                </div>                
+                
+            </div>
+            <div className={styles.box}>
+                 
                 <div className={styles.content}>
 
                 </div>
