f41d46b243bda80781c7622f75c6393f6da3d277
Thiago Salgado <thiago.salgado.monteiro@gmail.com>
2023-12-02 20:16:48 -0300

    [Hotfix] - Node 18

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -43,7 +43,6 @@ export function About() {
                         </div>
                     </div>
                 </div>
-                <div>teste</div>
             </section>
         </>
     )
