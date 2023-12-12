0a8f50ce64f7f34f60e086af4fa5a8120ced9d70
unknown <leonidio.ti@gmail.com>
2023-12-12 14:46:15 -0300

    submenu AboutUs started

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -3,13 +3,22 @@ import { Link } from 'react-router-dom';
 
 export function Navbar() {
     return (
-        <nav >           
-            <Link to= "/" className={styles.item}> Home </Link >
-            <Link to= "/sobre" className={styles.item}> Sobre Nós </Link >
-            <Link to= "/servicos" className={styles.item}> Serviços </Link >
-            <Link to= "/empresas" className={styles.item}> Empresas </Link >
-            <Link to= "/comunicacao" className={styles.item}> Comunicação </Link >
-            <Link to="/contato" className={styles.item}> Contato </Link >
+        <nav >  
+
+            <ul className={styles.menu}>
+                <li> <Link to= "/" className={styles.item}> Home </Link ></li>
+                <li>
+                    Sobre Nós 
+                    <ul className={styles.submenu}>
+                        <Link to= "/sobre" className={styles.item} > Equipe </Link >                        
+                        <Link to="/Consultores" className={styles.item}>Consultores</Link>
+                    </ul>
+                </li>
+                <li> <Link to= "/servicos" className={styles.item}> Serviços </Link ></li>
+                <li> <Link to= "/empresas" className={styles.item}> Empresas </Link ></li>
+                <li><Link to= "/comunicacao" className={styles.item}> Comunicação </Link ></li>
+                <li><Link to="/contato" className={styles.item}> Contato </Link ></li>
+            </ul>                     
             
         </nav>
     )
